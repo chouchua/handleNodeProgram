8fbe69cf9cdc7e36b4902e428f58ca4c678bf414
Jimmy Chou <cjimmy@ca.ibm.com>
2016-06-30 16:27:44 -0400

    initialize project

---
+++ index.js
@@ -0,0 +1,31 @@
+function handleProgram(args){
+    this.arguments = processArgs(args);
+    this.commands = {};
+    this.args = [];
+    
+}
+
+handleProgram.prototype.addCommand = function(name,fnc){
+    this.commands[name] = fnc;
+}
+
+handleProgram.prototype.call = function(functionName){
+    if (this.commands[functionName]!=null){
+        console.log('calling ' + functionName);
+    }
+    else{
+
+    }
+}
+
+function processArgs(args){
+    //this program takes up to three arguments, all optional parameters in coding perspective.
+    //var arg1 = || arg1;
+    var arguments = args.slice(2,args.length);
+    var length = arguments.length;
+    //how many arguments does this program want to handle?
+    console.log(arguments);
+    return arguments;
+}
+
+module.exports = handleProgram;
---
+++ test.js
@@ -0,0 +1,2 @@
+console.log("args: ",process.argv);
+var program = require('./handleProgram')(process.argv);
